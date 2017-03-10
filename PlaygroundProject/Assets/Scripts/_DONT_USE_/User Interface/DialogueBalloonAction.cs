bd59b7d33485630b56cc2f069ef58b512faaeedc
Ciro Continisio <ciro@unity3d.com>
2017-03-10 09:38:58 +0000

    Started work on dialogue system

---
+++ PlaygroundProject/Assets/Scripts/_DONT_USE_/User Interface/DialogueBalloonAction.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogueBalloonAction : Action
+{
+	public string textToDisplay;
+	public Color backgroundColor;
+	public Color textColor;
+
+	public DisappearMode disappearMode;
+	public float timeToDisappear;
+	public KeyCode keyToPress;
+
+
+
+	public enum DisappearMode
+	{
+		Time,
+		ButtonPress,
+	}
+}
