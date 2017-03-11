9638d94e7c09bbd6842e2f7593bd1010b8d8f5a0
Ciro Continisio <ciro@unity3d.com>
2017-03-11 20:23:46 +0000

    - Finished implementation of direction arrow - Moved _DONT_USE_ to root

--- PlaygroundProject/Assets/Scripts/Movement/AutoMove.cs
+++ PlaygroundProject/Assets/Scripts/Movement/AutoMove.cs
@@ -4,12 +4,9 @@ using System.Collections;
 [RequireComponent(typeof(Rigidbody2D))]
 public class AutoMove : Physics2DObject
 {
-	public Mesh m;
-
 	// These are the forces that will push the object every frame
 	// don't forget they can be negative too!
-	public float horizontalMovement = 1f;
-	public float verticalMovement = 0f;
+	public Vector2 direction = new Vector2(1f, 0f);
 
 
 	//is the push relative or absolute to the world?
@@ -21,11 +18,11 @@ public class AutoMove : Physics2DObject
 	{
 		if(relative)
 		{
-			rigidbody2D.AddRelativeForce(new Vector2(horizontalMovement, verticalMovement) * 2f);
+			rigidbody2D.AddRelativeForce(direction * 2f);
 		}
 		else
 		{
-			rigidbody2D.AddForce(new Vector2(horizontalMovement, verticalMovement) * 2f);
+			rigidbody2D.AddForce(direction * 2f);
 		}
 	}
 
@@ -33,15 +30,10 @@ public class AutoMove : Physics2DObject
 
 	void OnDrawGizmosSelected()
 	{
-		Gizmos.color = Color.green;
-		Vector2 orientation = new Vector2(horizontalMovement, verticalMovement);
-		float length = orientation.magnitude;
-		Gizmos.DrawWireMesh(m, transform.position, Quaternion.AngleAxis(Angle(orientation), Vector3.forward), Vector3.one * length);
-	}
-
-	public float Angle(Vector2 v)
-	{
-		if(v.x<0) return (Mathf.Atan2(v.x, v.y)*Mathf.Rad2Deg*-1)-360;
-		else return -Mathf.Atan2(v.x,v.y)*Mathf.Rad2Deg;
+		if(this.enabled)
+		{
+			float extraAngle = (relative) ? transform.rotation.eulerAngles.z : 0f;
+			Utils.DrawArrowGizmo(transform.position, direction, extraAngle);
+		}
 	}
 }
---
+++ PlaygroundProject/Assets/_DONT_USE_/Utilities/Utils.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class Utils
+{
+	public static Mesh arrowMesh;
+
+	static Utils()
+	{
+		arrowMesh = Resources.Load<Mesh>("Meshes/Arrow");
+	}
+
+	public static void SetAxisTowards(Enums.Directions axis, Transform t, Vector2 direction)
+	{
+		switch(axis)
+		{
+			case Enums.Directions.Up:
+				t.up = direction;
+				break;
+			case Enums.Directions.Down:
+				t.up = -direction;
+				break;
+			case Enums.Directions.Right:
+				t.right = direction;
+				break;
+			case Enums.Directions.Left:
+				t.right = -direction;
+				break;
+		}
+	}
+
+	//Draws a green arrow in a certain direction, with support for an extraAngle (to make it relative to the gameObject's rotation) and a specific scale
+	public static void DrawArrowGizmo(Vector3 position, Vector2 direction, float extraAngle, float scale = 0f)
+	{
+		Gizmos.color = Color.green;
+		float arrowAngle = Angle(direction);
+
+		if(scale == 0f)
+		{
+			//calculate it from the direction
+			scale = direction.magnitude;
+		}
+
+		Gizmos.DrawMesh(arrowMesh, position, Quaternion.AngleAxis(arrowAngle + extraAngle, Vector3.forward), Vector3.one * scale);
+	}
+
+	public static float Angle(Vector2 inputVector)
+	{
+		if(inputVector.x<0) return (Mathf.Atan2(inputVector.x, inputVector.y)*Mathf.Rad2Deg*-1)-360;
+		else return -Mathf.Atan2(inputVector.x,inputVector.y)*Mathf.Rad2Deg;
+	}
+}
