d68f95d38e8a17152d0736f39bc8930e8abd087a
Ciro Continisio <ciro@unity3d.com>
2018-01-21 00:42:17 +0000

    All Gizmo icons

---
+++ PlaygroundProject/Assets/_DONT_USE_/Editor/Export.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ExportAll
+{
+
+	[MenuItem("Playground/Export Unitypackage")]
+	public static void Export () {
+		string[] projectContent = AssetDatabase.GetAllAssetPaths();  
+		AssetDatabase.ExportPackage(projectContent, "UnityPlayground.unitypackage", ExportPackageOptions.Recurse | ExportPackageOptions.IncludeLibraryAssets );  
+		
+		Debug.Log("Project Exported");
+	}
+}
