71f3470d979bcd5e257ae1cbd38557dfcebb5e83
Ciro Continisio <ciro@unity3d.com>
2018-04-06 13:54:34 +0200

    Temp changes to prepare for new Help box in components

--- PlaygroundProject/Assets/UnityPlayground/_DONT_USE/Scripts/Editor/Conditions/ConditionKeyPressInspector.cs
+++ PlaygroundProject/Assets/UnityPlayground/_DONT_USE/Scripts/Editor/Conditions/ConditionKeyPressInspector.cs
@@ -7,6 +7,7 @@ using UnityEditorInternal;
 [CustomEditor(typeof(ConditionKeyPress))]
 public class ConditionKeyPressInspector : ConditionInspectorBase
 {
+	private bool t;
 	private string explanation = "Use this script to perform an action when a button is pressed, released, or as long as it's kept pressed (in this case you get to choose the frequency).";
 
 	public override void OnInspectorGUI()
@@ -15,7 +16,43 @@ public class ConditionKeyPressInspector : ConditionInspectorBase
 
 		GUILayout.Space(10);
 		EditorGUILayout.HelpBox(explanation, MessageType.Info);
+		
+		/*
+		Texture2D headerBackground;
+		GUIStyle g = new GUIStyle();
+		headerBackground = Resources.Load<Texture2D>("Textures/Blue");
+		g.normal.background = headerBackground;
+		EditorGUILayout.BeginVertical(g);
 
+		g.padding = new RectOffset(5,0,0,0);
+		if(!EditorGUIUtility.isProSkin)
+		{
+			headerBackground = Resources.Load<Texture2D>("Textures/HeaderPers");
+		}
+		else
+		{
+			headerBackground = Resources.Load<Texture2D>("Textures/GreyPro");
+		}
+		g.normal.background = headerBackground;
+
+		EditorGUILayout.BeginVertical(g);
+		EditorGUI.indentLevel++;
+		t = EditorGUILayout.Foldout(t, "Help");
+		if(t)
+		{
+			EditorGUI.indentLevel--;
+			//EditorGUILayout.HelpBox(explanation, MessageType.Info);
+			GUISkin s = Resources.Load<GUISkin>("Playground");
+			GUI.skin = s;
+			GUILayout.Label(explanation, "HelpText");
+			EditorGUI.indentLevel++;
+			GUI.skin = null;
+		}
+		EditorGUI.indentLevel--;
+        EditorGUILayout.EndVertical ();
+        EditorGUILayout.EndVertical ();
+		*/
+ 		
 		GUILayout.Space(10);
 		EditorGUILayout.PropertyField(serializedObject.FindProperty("happenOnlyOnce"));
 
