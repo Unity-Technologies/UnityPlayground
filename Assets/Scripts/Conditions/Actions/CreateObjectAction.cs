1efecdfdc2f0c26038baf3b07b34af1bbd9a1eee
AlexKostas <k.alexandros299@gmail.com>
2019-01-26 21:02:23 +0200

    Update CreateObjectActions.cs
    Added option to spawn an object after specified time delay in the CreateObjectAction script

--- Assets/Scripts/Conditions/Actions/CreateObjectAction.cs
+++ Assets/Scripts/Conditions/Actions/CreateObjectAction.cs
@@ -7,31 +7,53 @@ public class CreateObjectAction : Action
 	public GameObject prefabToCreate;
 	public Vector2 newPosition;
 	public bool relativeToThisObject;
+	public bool afterDelay;
+	public int secondsToWait = 1;
 		
 	// Creates a new GameObject
 	public override bool ExecuteAction(GameObject dataObject)
 	{
-		if(prefabToCreate != null)
+		if (prefabToCreate != null)
 		{
-			//create the new object by copying the prefab
-			GameObject newObject = Instantiate<GameObject>(prefabToCreate);
-
-			//is the position relative or absolute?
-			Vector2 finalPosition = newPosition;
-			if (relativeToThisObject)
+			if (afterDelay)
 			{
-				finalPosition = (Vector2)transform.position + newPosition;
+				StartCoroutine(SpawnAfterDelay());
 			}
-
-			//let's place it in the desired position!
-			newObject.transform.position = finalPosition;
+			else
+			{
+				SpawnObject();
+			}
+			
 			return true;
 		}
-		else
-		{
-			Debug.LogWarning("There is no Prefab assigned to this CreateObjectAction, so a new object can't be created.");
-			return false;
-		}
+
+		Debug.LogWarning("There is no Prefab assigned to this CreateObjectAction, so a new object can't be created.");
+		return false;
 	}
 
+	//Spawn the specified prefab in the scene
+	private void SpawnObject()
+	{
+		//create the new object by copying the prefab
+		GameObject newObject = Instantiate(prefabToCreate);
+
+		//is the position relative or absolute?
+		Vector2 finalPosition = newPosition;
+		if (relativeToThisObject)
+		{
+			finalPosition = (Vector2) transform.position + newPosition;
+		}
+
+		//let's place it in the desired position!
+		newObject.transform.position = finalPosition;
+	}
+
+	private IEnumerator SpawnAfterDelay()
+	{
+		//Wait for specified time
+		yield return new WaitForSecondsRealtime(secondsToWait);
+		
+		//Spawn the object
+		SpawnObject();
+	}
 }
