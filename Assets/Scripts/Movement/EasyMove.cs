54b8b18ebb41c31a6dc929ae3057f7f75d527409
tomotomospot <kinematoron_sas@yahoo.co.jp>
2019-03-23 12:05:10 +0900

    add File  Assets/Scripts/Movement/EasyMove.cs

---
+++ Assets/Scripts/Movement/EasyMove.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EasyMove : MonoBehaviour
+{
+	public float speed = 2.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    	if (Input.GetKey ("up")) {
+			transform.position += transform.forward * speed * Time.deltaTime;
+		}
+		if (Input.GetKey ("down")) {
+			transform.position -= transform.forward * speed * Time.deltaTime;
+		}
+		if (Input.GetKey("right")) {
+			transform.position += transform.right * speed * Time.deltaTime;
+		}
+		if (Input.GetKey ("left")) {
+			transform.position -= transform.right * speed * Time.deltaTime;
+		}
+        
+    }
+}
