198e813105db1befa684d7d92ebda49443320d30
tomonori kato <kinematoron_sas@yahoo.co.jp>
2019-03-23 12:10:25 +0900

    Update EasyMove.cs

--- Assets/Scripts/Movement/EasyMove.cs
+++ Assets/Scripts/Movement/EasyMove.cs
@@ -14,7 +14,7 @@ public class EasyMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-    	if (Input.GetKey ("up")) {
+    		if (Input.GetKey ("up")) {
 			transform.position += transform.forward * speed * Time.deltaTime;
 		}
 		if (Input.GetKey ("down")) {
